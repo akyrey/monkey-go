4d714b3fcd1c21a639957bdb5efd5805f44faa7e
akyrey <stein.dario@gmail.com>
2023-06-05 23:45:35 +0200

    feat: parse boolean expressions
    Got to page 82

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -171,18 +173,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 	leftExp := prefix()
 
-    // It repeats this loop again and again until it finds a SEMICOLON or it encounters a token with lower precedence
-    for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
-        infix := p.infixParseFns[p.peekToken.Type]
+	// It repeats this loop again and again until it finds a SEMICOLON or it encounters a token with lower precedence
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
 
-        if infix == nil {
-            return leftExp
-        }
+		if infix == nil {
+			return leftExp
+		}
 
-        p.nextToken()
+		p.nextToken()
 
-        leftExp = infix(leftExp)
-    }
+		leftExp = infix(leftExp)
+	}
 
 	return leftExp
 }
@@ -244,6 +246,10 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit.Value = value
 
 	return lit
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
 
 func (p *Parser) nextToken() {

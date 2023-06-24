e78e67c649ca281a6212bdc9c3848828168b2a16
akyrey <stein.dario@gmail.com>
2023-06-24 23:38:58 +0200

    feat: evaluate return statements
    Got to page 131

--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 // Representation of every Monkey value
@@ -50,4 +51,16 @@ func (n *Null) Type() ObjectType {
 }
 func (n *Null) Inspect() string {
 	return "null"
+}
+
+// Just a wrapper around an object, so we can keep track of it and can later decide whether to stop evaluation or not
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
 }
